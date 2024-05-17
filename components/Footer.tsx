5c595386231debb986108cd8a704bb85f9ec799d
jean.konan <jean.konan@inexa-ci.com>
2024-05-17 16:07:50 +0000

    Add a relative position and overflow-hidden to remove the large space in footer section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,10 @@ import MagicButton from "./MagicButton";
 
 const Footer = () => {
   return (
-    <footer className="w-full pt-20 pb-10" id="contact">
+    <footer
+      className="relative overflow-hidden w-full pt-20 pb-10"
+      id="contact"
+    >
       {/* background grid */}
       <div className="w-full absolute left-0 -bottom-72 min-h-96">
         <img
