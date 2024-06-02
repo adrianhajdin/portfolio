11afeacd22f1f38093e4416eb4772862b65010b6
Mohsen Mirzaei <mohsn.mirzaei@gmail.com>
2024-06-02 12:54:11 +0330

    Fix build error by implementing conditional dynamic import for Lottie

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,16 +1,22 @@
 import { useState } from "react";
 import { IoCopyOutline } from "react-icons/io5";
 
-// Also install this npm i --save-dev @types/react-lottie
-import Lottie from "react-lottie";
-
 import { cn } from "@/lib/utils";
-
 
 import { BackgroundGradientAnimation } from "./GradientBg";
 import GridGlobe from "./GridGlobe";
 import animationData from "@/data/confetti.json";
 import MagicButton from "../MagicButton";
+import dynamic from "next/dynamic";
+
+// Also install this npm i --save-dev @types/react-lottie
+let Lottie;
+
+if (process.env.NODE_ENV === "development") {
+  Lottie = require("react-lottie").default;
+} else {
+  Lottie = dynamic(() => import("react-lottie"), { ssr: false });
+}
 
 export const BentoGrid = ({
   className,
@@ -99,8 +105,9 @@ export const BentoGridItem = ({
           )}
         </div>
         <div
-          className={`absolute right-0 -bottom-5 ${id === 5 && "w-full opacity-80"
-            } `}
+          className={`absolute right-0 -bottom-5 ${
+            id === 5 && "w-full opacity-80"
+          } `}
         >
           {spareImg && (
             <img
@@ -176,8 +183,9 @@ export const BentoGridItem = ({
               {/* remove focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 */}
               {/* add handleCopy() for the copy the text */}
               <div
-                className={`absolute -bottom-5 right-0 ${copied ? "block" : "block"
-                  }`}
+                className={`absolute -bottom-5 right-0 ${
+                  copied ? "block" : "block"
+                }`}
               >
                 {/* <img src="/confetti.gif" alt="confetti" /> */}
                 <Lottie options={defaultOptions} height={200} width={400} />
