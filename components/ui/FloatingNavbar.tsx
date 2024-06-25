2bd96459fc60aa41056e0b1b102bf3e73e216b75
hakeem-jc <hakeemclarke14@gmail.com>
2024-06-25 18:16:35 -0500

    feat: Add Dribble

--- components/ui/FloatingNavbar.tsx
+++ components/ui/FloatingNavbar.tsx
@@ -72,6 +72,7 @@ export const FloatingNav = ({
             className={cn(
               "relative dark:text-neutral-50 items-center  flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
             )}
+            target="_blank"
           >
             <span className="block sm:hidden">{navItem.icon}</span>
             <span className=" text-sm !cursor-pointer">
