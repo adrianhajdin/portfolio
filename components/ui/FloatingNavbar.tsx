aea85c2e11c239590c3ae90c5c6337821cc7829e
hakeem-jc <hakeemclarke14@gmail.com>
2024-06-25 16:48:07 -0500

    refactor: Remove unused login button

--- components/ui/FloatingNavbar.tsx
+++ components/ui/FloatingNavbar.tsx
@@ -84,12 +84,7 @@ export const FloatingNav = ({
             {/* remove hidden sm:block for the mobile responsive */}
             <span className=" text-sm !cursor-pointer">{navItem.name}</span>
           </Link>
-        ))}
-        {/* remove this login btn */}
-        {/* <button className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
-          <span>Login</span>
-          <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
-        </button> */}
+        ))}        
       </motion.div>
     </AnimatePresence>
   );
