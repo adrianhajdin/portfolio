a377beed35e181dff89e8af7de08c5b70e69a80c
hakeem-jc <hakeemclarke14@gmail.com>
2024-06-25 17:44:01 -0500

    feat: Customize logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/jsm-logo.png" sizes="any" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
