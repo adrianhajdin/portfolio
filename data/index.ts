97dcd6cdcaca0fe2403b23335b856a0e80ae1ce2
hakeem-jc <hakeemclarke14@gmail.com>
2024-06-25 19:21:02 -0500

    refactor: Move links around

--- data/index.ts
+++ data/index.ts
@@ -1,8 +1,6 @@
 export const navItems = [
-  // { name: "About", link: "#about" },
-  // { name: "Projects", link: "#projects" },
-  { name: "Github", link: "https://github.com/hakeem-jc" },
   { name: "LinkedIn", link: "https://www.linkedin.com/in/hakeemclarke/" },
+  { name: "GitHub", link: "https://github.com/hakeem-jc" },
   { name: "Dribbble", link: "https://dribbble.com/HakeemC" },
 ];
 
