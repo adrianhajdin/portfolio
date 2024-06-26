d27cfa687498c7b262d9b3ee9b54bfe19f9e50ce
hakeem-jc <hakeemclarke14@gmail.com>
2024-06-25 19:23:31 -0500

    bugfix: Open links in new tab

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -44,6 +44,7 @@ const Footer = () => {
               key={info.id}
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
               href={info.src}
+              target="_blank"
             >
               <img src={info.img} alt="icons" width={20} height={20} />
             </a>
