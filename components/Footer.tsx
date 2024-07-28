3cd3d81bce78c46214d126c5897138340e50e895
Andreas Billy Sutandi <andreas.sutandi@atibusinessgroup.com>
2024-07-28 13:35:22 +0700

    fix: footer background, project navigation link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <footer className="w-full pt-20 pb-10" id="contact">
       {/* background grid */}
-      <div className="w-full absolute left-0 -bottom-72 min-h-96">
+      <div className="w-full absolute left-0 bottom-0 min-h-96">
         <img
           src="/footer-grid.svg"
           alt="grid"
--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -7,7 +7,7 @@ import { PinContainer } from "./ui/Pin";
 
 const RecentProjects = () => {
   return (
-    <div className="py-20">
+    <div className="py-20" id="projects">
       <h1 className="heading">
         A small selection of{" "}
         <span className="text-purple">recent projects</span>
