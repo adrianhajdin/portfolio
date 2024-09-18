2879507b7f4a4438e891ba8cd627b2e6f996ffb0
Stone Werner <stonewerner@berkeley.edu>
2024-09-17 21:22:36 -0500

    fixed scroll problem with mt-48

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 const Hero = () => {
   return (
-    <div className="pb-20 pt-36">
+    <div className="pb-20 pt-36 mt-48">
       {/**
        *  UI: Spotlights
        *  Link: https://ui.aceternity.com/components/spotlight
