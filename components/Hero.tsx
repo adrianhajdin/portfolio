61c725f2aed1d420d8015e1d50ebec3d596223a3
codingwithfelony <qasimshahzad1261@gmail.com>
2024-10-02 07:26:35 -0700

    Changes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -52,12 +52,12 @@ const Hero = () => {
            *  change md:text-6xl, add more responsive code
            */}
           <TextGenerateEffect
-            words="Transforming Concepts into Seamless User Experiences"
-            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Code. Create. Conquer."
+            className="text-center text-[50px] md:text-5xl lg:text-6xl"
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
+            Welcome to SYGNLS, Dev wizards conjuring digital magic.
           </p>
 
           <a href="#about">
