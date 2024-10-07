c2b7cf7ffed05eab6b6d80b8f6db355a76b70e89
Turab Manzoor <turabmanzoor032@gmail.com>
2024-10-07 22:12:02 +0530

    updated email

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,7 +24,7 @@ const Footer = () => {
           Reach out to me today and let&apos;s discuss how I can help you
           achieve your goals.
         </p>
-        <a href="mailto:contact@jsmastery.pro">
+        <a href="mailto:turabmanzoor52@gmail.com">
           <MagicButton
             title="Let's get in touch"
             icon={<FaLocationArrow />}
