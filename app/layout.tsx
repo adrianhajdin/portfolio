ecc1a25a71f0112602f2e328dec60bd5b9bae9fc
Raghav Goel <raghavgoel@Raghavs-MacBook-Air.local>
2024-11-07 21:57:48 +0530

    Hero Revamp

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "Raghav's Portfolio",
+  description: "My Web Dev Portfolio",
 };
 
 export default function RootLayout({
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -42,22 +42,15 @@ const Hero = () => {
 
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-          <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Web Magic with Next.js
-          </p>
-
-          {/**
-           *  Link: https://ui.aceternity.com/components/text-generate-effect
-           *
-           *  change md:text-6xl, add more responsive code
-           */}
           <TextGenerateEffect
-            words="Transforming Concepts into Seamless User Experiences"
+            words="Welcome! Let's turn your ideas into impactful digital experiences."
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
+            Hi! I&apos;m Raghav, a Web Developer passionate about building
+            innovative, user-centric websites that drive results and elevates
+            digital experiences.
           </p>
 
           <a href="#about">
